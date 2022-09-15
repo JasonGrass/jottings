265cb20832265f2d63dfead09092432c5174bca6
JasonGrass <junjiegrass@qq.com>
2022-09-15 22:53:26 +0800

    生成合并的 markdown

--- press/logseq/markdownFileMerger.js
+++ press/logseq/markdownFileMerger.js
@@ -1,20 +1,55 @@
 const path = require("path");
 const fs = require("fs");
-const { readFil, writeFile } = require("fs/promises");
+const _ = require("lodash");
+const {
+  readFile,
+  writeFile,
+  open,
+  unlink,
+  appendFile,
+} = require("fs/promises");
+
+const { isFileExisted } = require("./utils/io");
 
 /**
  * 合并所有的 markdown 文件
  * @param {Array} fileItems
  */
 async function mergeMarkdownFiles(fileItems, options) {
-  let content = "";
-  for (item of fileItems) {
-    const text = await readFile(item.file);
-    content += text;
+  const targetFile = path.join(options.dir, options.fileName);
+  if (await isFileExisted(targetFile)) {
+    await unlink(targetFile);
   }
 
-  const targetFile = path.join(options.dir, options.fileName);
-  await writeFile(targetFile, content);
+  await appendFile(targetFile, `#${options.title}\r\n`);
+
+  for (item of fileItems) {
+    await appendFile(
+      targetFile,
+      `\r\n\r\n# ${item.year} 年 ${item.month} 月 ${item.day} 日\r\n`
+    );
+
+    const text = await readFile(item.file);
+
+    await appendFile(targetFile, removeTextPrefix(text));
+  }
+}
+
+/**
+ * 移除 logseq 文本前面的 `-`
+ *
+ */
+function removeTextPrefix(text) {
+  const lines = _.split(text, "\n");
+  let content = "";
+  _.forEach(lines, (line) => {
+    line = _.replace(line, "\t", "  ");
+    line = _.trimStart(line, "-");
+    line = _.trimEnd(line, "\n");
+    content += `${line}\r\n`;
+  });
+
+  return content;
 }
 
 module.exports = {
--- press/logseq/utils/io.js
+++ press/logseq/utils/io.js
@@ -1,7 +1,6 @@
-
 /**
  * 判断文件夹是否存在
-*/
+ */
 function isFolderExisted(path_way) {
   return new Promise((resolve, reject) => {
     fs.access(path_way, (err) => {
@@ -19,6 +18,24 @@ function isFolderExisted(path_way) {
   });
 }
 
+function isFileExisted(path_way) {
+  return new Promise((resolve, reject) => {
+    fs.access(path_way, (err) => {
+      if (err) {
+        resolve(false); //"不存在"
+      } else {
+        var info = fs.statSync(path_way);
+        if (info.isFile()) {
+          resolve(true); //"存在"
+        } else {
+          resolve(false); //"不存在"
+        }
+      }
+    });
+  });
+}
+
 module.exports = {
-    isFolderExisted
+  isFolderExisted,
+  isFileExisted,
 };
