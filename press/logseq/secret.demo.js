f8658a983f4e395ac188f36c1144117991620f01
JasonGrass <junjiegrass@qq.com>
2022-09-15 09:50:26 +0800

    更新 secret.demo

--- press/logseq/secret.demo.js
+++ press/logseq/secret.demo.js
@@ -1,7 +1,7 @@
 module.exports = {
-    logseqPathStandby: [
-      `C:\Users\XXX\OneDrive\XXX\LogseqRepo\jottings`,
-      `C:\Users\XXX\OneDrive\XXX\LogseqRepo\jottings2`,
-    ],
-  };
-  
+  // logseq 的本地保存目录
+  logseqPathStandby: [
+    `C:\\Users\\XXX\\OneDrive\\XXX\\LogseqRepo\\jottings`,
+    `C:\\Users\\XXX\\OneDrive\\XXX\\LogseqRepo\\jottings2`,
+  ],
+};
